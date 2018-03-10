3aa8ce93ce5d1bc0d922b581b6621ddc3d7f1038
Alex Toft <35802824+alextoft@users.noreply.github.com>
2018-03-10 20:12:24 +0000

    Update getCurfewStatus.php

--- getCurfewStatus.php
+++ getCurfewStatus.php
@@ -6,6 +6,7 @@ $ch = curl_init($endpoint."/api/device/$flap/control");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Bearer $token"));
 $result = json_decode(curl_exec($ch),true) or die("Curl Failed\n");
+
 if($result['data']) {
 	if($result['data']['curfew']['enabled']=="true") {
 		print "Curfew for \"$flapname\": Enabled\n";
@@ -17,4 +18,3 @@ if($result['data']) {
 }
 
 ?>
-
