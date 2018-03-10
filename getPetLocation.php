6051df38ff2f98ce30e0ea38df719700d72231c3
Alex Toft <35802824+alextoft@users.noreply.github.com>
2018-03-10 20:11:20 +0000

    Update getPetLocation.php

--- getPetLocation.php
+++ getPetLocation.php
@@ -6,6 +6,7 @@ $ch = curl_init($endpoint."/api/pet/$pet/position");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Bearer $token"));
 $result = json_decode(curl_exec($ch),true) or die("Curl Failed\n");
+
 if($result['data']) {
 	if($result['data']['where']=="1") {
 		print "$petname's Current Location: Inside\n";
