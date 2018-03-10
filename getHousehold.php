7fc5769c935d175d312852515b07b064019924b9
Alex Toft <35802824+alextoft@users.noreply.github.com>
2018-03-10 20:13:09 +0000

    Update getHousehold.php

--- getHousehold.php
+++ getHousehold.php
@@ -6,6 +6,7 @@ $ch = curl_init($endpoint."/api/household");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Bearer $token"));
 $result = json_decode(curl_exec($ch),true) or die("Curl Failed\n");
+
 if($result['data']) {
 	print "Household ID: ".$result['data'][0]['id']."\nHousehold Name: ".$result['data'][0]['name']."\n";
 	$household = $result['data'][0]['id'];
@@ -14,4 +15,3 @@ if($result['data']) {
 }
 
 ?>
-
