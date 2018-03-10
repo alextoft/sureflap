b1f181c72babe2b73cc417cf564210a11e97d2df
Alex Toft <35802824+alextoft@users.noreply.github.com>
2018-03-10 20:11:58 +0000

    Update logout.php

--- logout.php
+++ logout.php
@@ -3,11 +3,13 @@
 if(!$oldtoken) {
 	$oldtoken = file_get_contents("token") or die("No token to logout!\n");
 }
+
 $ch = curl_init($endpoint."/api/auth/logout");
 curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json","Authorization: Bearer $oldtoken"));
 $result = curl_exec($ch);
+
 if(curl_getinfo($ch, CURLINFO_HTTP_CODE)=="200") {
 	print "Success!\n";
 } else {
@@ -15,4 +17,3 @@ if(curl_getinfo($ch, CURLINFO_HTTP_CODE)=="200") {
 }
 
 ?>
-
