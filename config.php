4db4aa724078fde2c7effde64d62422b5d215524
Alex Toft <35802824+alextoft@users.noreply.github.com>
2018-03-10 20:20:10 +0000

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,10 @@
 
 $email_address = "your.email@somewhere.com";
 $password = "LetMeIn";
+
 $endpoint = "https://app.api.surehub.io";
+
+// Invent something for mandatory fingerprintJs login value. Any 32bit integer will suffice.
 $device_id = (string) rand(1000000000,9999999999);
 
 ?>
