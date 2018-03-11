0078fc9c293a3a80b047e9f968375b6d149c2e00
Alex Toft <35802824+alextoft@users.noreply.github.com>
2018-03-11 18:52:26 +0000

    Add files via upload

---
+++ setEnableCurfew.php
@@ -0,0 +1,24 @@
+<?php
+
+if(!$argv[2]) {
+        die("Usage: php ".$_SERVER['PHP_SELF']." lockTime unlockTime (eg. 18:00 06:00)\n");
+}
+
+include_once 'getDevices.php';
+	
+$json = json_encode(array("curfew" => array("enabled" => true, "lock_time" => "$argv[1]", "unlock_time" => "$argv[2]")));
+$ch = curl_init($endpoint."/api/device/$flap/control");
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json","Content-Length: ".strlen($json),"Authorization: Bearer $token"));
+$result = json_decode(curl_exec($ch),true) or die("Curl Failed\n");
+
+if($result['data']['curfew']['enabled']==true) {
+	print "Successfully Enabled Curfew For \"$flapname\" between $argv[1] and $argv[2]\n";
+} else {
+	die("Enable Curfew Failed!\n");
+}
+
+?>
+
