48fe7dfb942b17e8cff30e9e9372ffc7207cae92
Mattheo1985 <matthias.reinhardt@gmx.de>
2018-07-11 21:47:36 +0200

    Fixed getCurfewStatus

--- getCurfewStatus.php
+++ getCurfewStatus.php
@@ -2,16 +2,16 @@
 
 include_once 'getDevices.php';
 
-$ch = curl_init($endpoint."/api/device/$flap/control");
+$ch = curl_init($endpoint."/api/household/$household/device?with[]=control");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Bearer $token"));
 $result = json_decode(curl_exec($ch),true) or die("Curl Failed\n");
 
 if($result['data']) {
-	if($result['data']['curfew']['enabled']=="true") {
+	if($result['data'][1]['control']['curfew']['enabled']=="true") {
 		print "Curfew for \"$flapname\": Enabled\n";
-		print "Curfew Lock Time: ".$result['data']['curfew']['lock_time']."\n";
-		print "Curfew Unlock Time: ".$result['data']['curfew']['unlock_time']."\n";
+		print "Curfew Lock Time: ".$result['data'][1]['control']['curfew']['lock_time']."\n";
+		print "Curfew Unlock Time: ".$result['data'][1]['control']['curfew']['unlock_time']."\n";
 	} else {
 		print "Curfew for \"$flapname\": Disabled\n";
 	}
